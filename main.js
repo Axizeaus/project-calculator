ee55f6a0c0956535c9fb6f958c60c86b1aa1cce2
Axizeaus <axizeaus@gmail.com>
2022-07-30 05:27:31 +0630

    Add displayOnScreen()

--- main.js
+++ main.js
@@ -4,6 +4,15 @@ const addId = document.getElementById('add');
 const subtractId = document.getElementById('subtract');
 const multiplyId = document.getElementById('multiply');
 const divideId = document.getElementById('division');
+const mainScreen = document.getElementById('main-screen');
+const numbers = document.getElementById('numbers');
+
+
+// console.log(numbers.children[0].textContent);
+for (let i of numbers.children){
+    console.log(i);
+    i.onclick = (e) => displayOnScreen(e.target.textContent); 
+}
 
 // events 
 
@@ -30,3 +39,9 @@ function operate(func,x,y){
     console.log(func, x, y)
     return func(x,y);
 }
+
+function displayOnScreen(val){
+    console.log(val);
+    mainScreen.textContent += val;
+}
+
